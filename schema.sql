cff6b8fbb673e679b16343262e0ab4c77cef5ecc
Erik Vormala <erik.vormala@helsinki.fi>
2025-12-02 22:06:58 +0200

    Add pylint

--- schema.sql
+++ schema.sql
@@ -4,15 +4,14 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE boards (
+    id INTEGER PRIMARY KEY,
+    board MATRIX 
+);
+
 CREATE TABLE matches (
     id INTEGER PRIMARY KEY,
     player_id1 INTEGER REFERENCES users,
     player_id2 INTEGER REFERENCES users,
+    board_id INTEGER REFERENCES boards
 );
-
-CREATE TABLE boards (
-    id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT
-);
-
